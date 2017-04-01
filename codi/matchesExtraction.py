cd980a30ed83b290a1bcac34e546b8f917a71c1c
Wildux <wildux@users.noreply.github.com>
2017-04-01 22:55:45 +0200

    Experiments

--- codi/matchesExtraction.py
+++ codi/matchesExtraction.py
@@ -5,11 +5,15 @@ import cv2
 import scipy as sp
 
 #EXECUCIÓ
-imgs1 = ['images/experiments/uni_2.jpg', 'images/experiments/uni4_2.jpg', 'images/experiments/motos3.png', 'images/experiments/cars4.png', 'images/experiments/uni1.jpg', 'images/experiments/jardi_2.jpg']
-imgs2 = ['images/experiments/uni.jpg', 'images/experiments/uni4.jpg', 'images/experiments/motos1.png', 'images/experiments/cars6.png', 'images/experiments/uni2.jpg', 'images/experiments/jardi2.jpg']
-algs1 = [vc._HARRIS, vc._HARRIS, vc._SIFT, vc._ORB, vc._ORB]
-algs2 = [vc._SIFT, vc._ORB, vc._SIFT, vc._ORB, vc._BRISK]
-algsName = ["HARRIS + SIFT", "HARRIS + ORB", "SIFT", "ORB", "ORB + BRISK"]
+imgs1 = ['images/experiments/uni_2.jpg', 'images/experiments/uni4_2.jpg', 'images/experiments/motos3.png', 'images/experiments/cars4.png',
+		'images/experiments/uni1.jpg', 'images/experiments/jardi_2.jpg', 'images/experiments/jardi_sel.jpg', 'images/experiments/uni_sel.jpg']
+
+imgs2 = ['images/experiments/uni.jpg', 'images/experiments/uni4.jpg', 'images/experiments/motos1.png', 'images/experiments/cars6.png',
+		'images/experiments/uni2.jpg', 'images/experiments/jardi2.jpg', 'images/experiments/jardi_2.jpg', 'images/experiments/uni1.jpg']
+
+algs1 = [vc._HARRIS, vc._SIFT, vc._ORB, vc._ORB]
+algs2 = [vc._SIFT, vc._SIFT, vc._ORB, vc._BRISK]
+algsName = ["HARRIS_SIFT", "SIFT", "ORB", "ORB_BRISK"]
 
 def printMatches(img1, img2, k1, k2, sel_matches, name):
 	h1, w1 = img1.shape[:2]
@@ -25,7 +29,7 @@ def printMatches(img1, img2, k1, k2, sel_matches, name):
 	num = 0
 	for m in sel_matches:
 		cv2.line(view, (int(k1[m.queryIdx].pt[0]), int(k1[m.queryIdx].pt[1])) , (int(k2[m.trainIdx].pt[0] + w1), int(k2[m.trainIdx].pt[1])), color, 5)
-		cv2.imwrite("resultats/2.2/"+name+"_"+str(num)+".png", view)
+		cv2.imwrite("resultats/2.2/"+name+"/"+str(num)+".png", view)
 		view = cp.copy()
 		num = num + 1
 
@@ -50,7 +54,24 @@ for z in range(len(imgs1)):
 		kpRobot = vc.point_selection(imgRobotGray, psAlg)					# Get points
 		kp2, desRobot = vc.feature_extraction(imgRobotGray, kpRobot, feAlg)	# Get features
 
-		good = vc.matching2(imgROI, imgRobot, desROI, desRobot, kp1, kp2, feAlg)	# Find matching point
+		matches = vc.matching(imgROI, imgRobot, desROI, desRobot, kp1, kp2, feAlg)	# Find matching point
 
-		printMatches(imgROI, imgRobot, kp1, kp2, good, algsName[i]+"_"+str(z))
+		if len(matches) >= 10:
+			src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+			dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+			M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+			matchesMask = mask.ravel().tolist()
+
+			good_matches = []
+			discarded_matches = []
+			for m in range(len(matchesMask)):
+				if matchesMask[m] == 1:
+					good_matches.append(matches[m])
+				else:
+					discarded_matches.append(matches[m])
+		else:
+			good_matches = matches
+			discarded_matches = []
+		printMatches(imgROI, imgRobot, kp1, kp2, good_matches, algsName[i]+"/"+str(z))
+		printMatches(imgROI, imgRobot, kp1, kp2, discarded_matches, algsName[i]+"/discarded/"+str(z))
 	print("\n")
--- codi/timeExtraction.py
+++ codi/timeExtraction.py
@@ -10,9 +10,9 @@ imgs1 = ['images/experiments/uni_2.jpg', 'images/experiments/uni4_2.jpg', 'image
 imgs2 = ['images/experiments/uni.jpg', 'images/experiments/uni4.jpg', 'images/experiments/motos1.png', 'images/experiments/cars6.png',
 		'images/experiments/uni2.jpg', 'images/experiments/jardi2.jpg', 'images/experiments/jardi_2.jpg', 'images/experiments/uni1.jpg']
 
-algs1 = [vc._HARRIS, vc._HARRIS, vc._SIFT, vc._ORB, vc._ORB]
-algs2 = [vc._SIFT, vc._ORB, vc._SIFT, vc._ORB, vc._BRISK]
-algsName = ["HARRIS + SIFT", "HARRIS + ORB", "SIFT", "ORB", "ORB + BRISK"]
+algs1 = [vc._HARRIS, vc._SIFT, vc._ORB, vc._ORB]
+algs2 = [vc._SIFT, vc._SIFT, vc._ORB, vc._BRISK]
+algsName = ["HARRIS_SIFT", "SIFT", "ORB", "ORB_BRISK"]
 
 
 for z in range(len(imgs1)):
@@ -52,20 +52,28 @@ for z in range(len(imgs1)):
 			matchesMask = mask.ravel().tolist()
 
 			good_matches = []
+			discarded_matches = []
 			for m in range(len(matchesMask)):
 				if matchesMask[m] == 1:
 					good_matches.append(matches[m])
+				else:
+					discarded_matches.append(matches[m])
 		else:
 			good_matches = matches
+			discarded_matches = []
 
 		draw_params = dict(matchColor = (0,255,0), singlePointColor = None, flags = 2)
 		img3 = cv2.drawMatches(imgROI,kp1,imgRobot,kp2,good_matches,None,**draw_params)
-		#cv2.imwrite("resultats/2.1/matches_"+algsName[i]+"_"+str(z)+".png", img3)
+		img4 = cv2.drawMatches(imgROI,kp1,imgRobot,kp2,discarded_matches,None,**draw_params)
+		cv2.imwrite("resultats/2.1/ransac_matches/"+algsName[i]+"/"+str(z)+".png", img3)
+		cv2.imwrite("resultats/2.1/discarded_matches/"+algsName[i]+"/"+str(z)+".png", img4)
 
 		x, y, img3 = vc.homography(imgROI, imgRobot, desROI, desRobot, kp1, kp2, matches)	# Find matching point
-		#cv2.imwrite("resultats/2.1/homography_"+algsName[i]+"_"+str(z)+".png", img3)
+		cv2.imwrite("resultats/2.1/homography/"+algsName[i]+"/"+str(z)+".png", img3)
+		img4 = cv2.circle(imgRobot,(int(x),int(y)), 5, (0,0,255), -1)
+		cv2.imwrite("resultats/2.1/punts/"+algsName[i]+"/"+str(z)+".png", img4)
 
-		#print(name, np.mean(times), "Good matches: "+str(len(good_matches)))
-		print(name, np.mean(times), "Good matches: "+str(len(matches)))
+		print(name, np.mean(times), "Ransac matches: "+str(len(good_matches)))
+		print(name, np.mean(times), "Total matches: "+str(len(matches)))
 	print("\n")
 
